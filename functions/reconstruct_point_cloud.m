38aa8e9922d23d42878808ee7b1c771edf143847
Ludwig Pethrus Engstrom <ludwigpet@gmail.com>
2014-09-15 22:24:12 +0200

    Reconstruct point cloud done!

--- functions/reconstruct_point_cloud.m
+++ functions/reconstruct_point_cloud.m
@@ -20,4 +20,26 @@ function points3d = reconstruct_point_cloud( cameras, points2d )
 %------------------------------
 % TODO: FILL IN THIS PART
 
+C = 2;
+[~,N,~] = size(points2d);
+points3d = zeros(4,N);
+for i = 1:N
+    M = [];
+    for j = 1:C
+        x = points2d(1,i,j);
+        y = points2d(2,i,j);
+        
+        m1 = cameras(1,:,j);
+        m2 = cameras(2,:,j);
+        m3 = cameras(3,:,j);
+        r1 = xa*ma3-(1*m1)
+        r2 = ya*ma3-(1*m2)
+        M = [M;r1;r2];
+    end
+   [U,S,V] = svd(M)
+   p3d = V(:,end);
+   points3d(:,i) = p3d;
+end
+
+
 
